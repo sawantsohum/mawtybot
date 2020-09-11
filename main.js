66e3cfc30c685789de89f6eb39d87c6cf887e35b
sawantsohum <sawantsohum@gmail.com>
2020-09-11 18:06:34 -0500

    sdfsdfjsjdfjsfjd

--- main.js
+++ main.js
@@ -285,6 +285,7 @@ function play(guild, song) {
     })
     .on("error", error => console.error(error));
   dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+  list()
   serverQueue.textChannel.send(`Start playing: **${song.title}** \n make sure to enter ~stop once you are done listening to music to save on server costs :) <3`);
 }
 client.login(token);
